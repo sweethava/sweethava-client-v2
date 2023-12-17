0ce35367948dff392ddc5c8649245a6c1c85af42
Zhan Ilyas <ilyasov.zhan@gmail.com>
2023-12-17 19:23:54 +0600

    Skills list change

--- src/components/ui/desktopScreen/skillsList/skillsList.tsx
+++ src/components/ui/desktopScreen/skillsList/skillsList.tsx
@@ -1,13 +1,16 @@
 'use client'
 
-import { DesktopSkillsList } from '../../../../config/desktopConfig'
 import styles from './skillsList.module.scss'
 
-const SkillsList = () => {
+interface SkillsListProps {
+    skills: string[]
+}
+
+const SkillsList = ({ skills }: SkillsListProps) => {
     return (
         <div className={styles.list}>
             {
-                DesktopSkillsList.map((text, index) => {
+                skills.map((text, index) => {
                     return <div className={styles.item} key={index}>{text}</div>
                 })
             }
